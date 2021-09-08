ada4eef54d1ddbefd40d6da548a599bbd68bb7dd
Fran <fran199017@gmail.com>
2021-09-08 12:52:35 +0200

    Añadido a templates la visualizacion de las fotos subidas

--- src/main/java/com/fran/app/controllers/ClienteController.java
+++ src/main/java/com/fran/app/controllers/ClienteController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,6 +35,20 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	@GetMapping(value="/ver/{id}")
+	public String ver(@PathVariable(value="id") Long id, Map<String,Object> model, RedirectAttributes flash) {
+		
+		Cliente cliente= clienteService.findOne(id);
+		if(cliente==null) {
+			flash.addFlashAttribute("error","El cliente no existe en la BBDD");
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo","Detalle Cliente: " + cliente.getNombre());
+		
+		return "ver";
+	}
 	
 	@RequestMapping(value="/listar", method= RequestMethod.GET)
 	public String listar(@RequestParam(name="page", defaultValue="0")  int page, Model model) {
